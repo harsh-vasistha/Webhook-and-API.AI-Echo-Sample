6dede22e2847d0636c0d0b982b1ce9d36f6c3bbb
Harsh Vasistha <harsh.vasistha@outlook.com>
2018-12-05 05:58:09 +0530

    Fix for v2
    Signed-off-by: Harsh Vasistha <harsh.vasistha@outlook.com>

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ restService.post("/echo", function(req, res) {
   return res.json({
     speech: speech,
     fulfillmentText: speech,
+    payload : { google : { expectUserResponse : true, richResponse : {items : [{simpleResponse : {textToSpeech : speech}}]}} },
     source: "webhook-echo-sample"
   });
 });
